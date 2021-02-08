ffb0d9b5bbc93c7275c4bd2644d32571d5c33120
AmandaKhol <amanda.camsua@gmail.com>
2021-02-08 13:34:15 +0100

    add palettes exercise functionalities

--- m2/repaso-paletas-color/modulo-2-repaso-final-paletas/src/js/main.js
+++ m2/repaso-paletas-color/modulo-2-repaso-final-paletas/src/js/main.js
@@ -3,7 +3,9 @@
 const palettesContainer = document.querySelector('.js-palettes-container')
 const filterInput = document.querySelector('.js-filter');
 const formElement = document.querySelector('.js-form');
+const palettesElement = document.querySelectorAll('.js-palette')
 let palettes = [];
+let favorites = [];
 
 // api
 function getDataFromApi() {
@@ -44,15 +46,27 @@ function handleForm(ev) {
 
 filterInput.addEventListener('keyup', handleFilter);
 formElement.addEventListener('submit', handleForm);
-//render function
 
+//render function
 function paintPalettes() {
     let htmlCode = '';
     for (const palette of palettes) {
-        if (palette.name.includes(filterInput.value)) {
-            htmlCode += '<li class="palette">';
-        } else { htmlCode += '<li class="palette palette-hidden">'; }
-
+        let isValidClass;
+        if (isValidPalette(palette)) {
+            isValidClass = '';
+        } else {
+            isValidClass = 'palette--hidden';
+        }
+        let isFavoriteClass;
+        if (isFavoritePalette(palette)) {
+            isFavoriteClass = 'palette--favorite';
+        } else {
+            isFavoriteClass = '';
+        }
+        /*         if (isValidPalette(palette)) {
+                    htmlCode += `<li class="palette js-palette" id="${palette.id}">`;
+                } else { htmlCode += `<li class="palette palette--hidden js-palette" id="${palette.id}>`; } */
+        htmlCode += `<li class="palette js-palette ${isValidClass} ${isFavoriteClass}" id="${palette.id}">`;
         htmlCode += `<h2 class="palette__title">${palette.name}</h2>`;
         htmlCode += `<div class="palette__colors">`;
         for (const paletteColor of palette.colors) {
@@ -62,6 +76,56 @@ function paintPalettes() {
         htmlCode += '</li>';
     }
     palettesContainer.innerHTML = htmlCode;
+    //tiene que estar aquí porque es aquí cuando ya se ha creado
+    listenPaletteEvents();
+}
+
+
+function isValidPalette(palette) {
+    return palette.name.toLowerCase().includes(filterInput.value.toLowerCase());
+}
+
+function isFavoritePalette(palette) {
+    const favoriteFound = favorites.find((favorite) => {
+        return favorite.id === palette.id
+    });
+    if (favoriteFound === undefined) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// listen palette events
+
+function listenPaletteEvents() {
+    const paletteElements = document.querySelectorAll('.js-palette');
+    for (const paletteElement of paletteElements) {
+        paletteElement.addEventListener("click", handlePalette);
+    }
+
+}
+
+function handlePalette(ev) {
+
+    const clickedPaletteId = ev.currentTarget.id;
+    const favoritesFoundIndex = favorites.findIndex(function (favorite) {
+        return favorite.id === clickedPaletteId;
+    });
+
+    if (favoritesFoundIndex === -1) {
+        //push: necesito la paleta. Añado favorito a la lista
+        const paletteFound = palettes.find(function (palette) {
+            return palette.id === clickedPaletteId;
+        });
+        favorites.push(paletteFound);
+    } else {
+        favorites.splice(favoritesFoundIndex, 1);
+    }
+
+
+    paintPalettes();
+
 }
 
 // start app
